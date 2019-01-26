a81f0f9ddd6f34edebe2de242e30884199989eb2
Luong Quang Manh <luongquangmanh85@gmail.com>
2019-01-26 14:05:29 +0700

    Remove resSetHeaders middleware of scrapers

--- packages/scrapers/src/index.js
+++ packages/scrapers/src/index.js
@@ -2,11 +2,9 @@ const { send } = require('micro')
 const Router = require('router')
 const finalhandler = require('finalhandler')
 
-const resSetHeaders = require('./middlewares/resSetHeaders')
 const apiRouter = require('./api')
 
 const router = new Router()
-router.use(resSetHeaders)
 router.use('/api', apiRouter)
 router.all('/', (req, res) =>
   send(res, 200, { message: 'Hello world', iam: '/index' })
--- packages/scrapers/src/middlewares/resSetHeaders.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-  res.setHeaders = (headers = {}) => {
-    Object.entries(headers).forEach(([k, v]) => {
-      res.setHeader(k, v)
-    })
-  }
-  next()
-}
