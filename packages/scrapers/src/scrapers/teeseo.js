ee09b7abbd03eb3213fef0a7b276b159a5ad9ea0
hoangkimminh <41377837+hoangkimminh@users.noreply.github.com>
2019-02-18 00:20:06 +0700

    Update teeseo.js

--- packages/scrapers/src/scrapers/teeseo.js
+++ packages/scrapers/src/scrapers/teeseo.js
@@ -18,6 +18,37 @@ module.exports = class TeeseoScraper extends GenericScraper {
       const relativePath = $(e).attr('href')
       nextUrls.push(`https://www.teeseo.top${relativePath}`)
     })
-    return { data: { id, title, description: title, price }, nextUrls }
+    const productTypes = []
+    $('#product_types')
+      .find('option')
+      .each((i, e) => {
+        productTypes.push($(e).attr('value'))
+      })
+
+    const colors = []
+    $('#product_colors')
+      .find('option')
+      .each((i, e) => {
+        colors.push($(e).attr('value'))
+      })
+
+    const imageLinks = []
+    $('#product_designs > div')
+      .find('img')
+      .each((i, e) => {
+        imageLinks.push($(e).attr('data-src'))
+      })
+
+    const variants = []
+    for (let i = 0; i < productTypes.length; i++) {
+      for (let j = 0; j < colors.length; j++) {
+        variants.push({
+          type: productTypes[i],
+          color: colors[j],
+          image: imageLinks[j]
+        })
+      }
+    }
+    return { data: { id, title, description: title, price, variants }, nextUrls }
   }
 }
